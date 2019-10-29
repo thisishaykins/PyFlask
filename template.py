e52bae3ca17e09b56067d194ebc58c5ce6bfca76
Akinshola Samuel <akinsholasamuel@gmail.com>
2019-10-29 13:05:58 +0100

    Initial commit: app base and basic template

---
+++ template.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/page/<name>")
+def page(name):
+    return render_template("index.html", content = name, r = 2, users = ["Akin", "Samuel", "Bill"])
+
+
+if __name__ == "__main__":
+    app.run()
